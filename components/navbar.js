b933dec8f587f21a60882f777cc687f9402fc2c6
Monolana <naufalx26@gmail.com>
2024-10-14 21:42:15 +0700

    testing anker component to the main page

---
+++ components/navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div>
+      <nav className={"bg-primary fixed w-full top-0 py-2 px-8"}>
+        <p>this is navbar</p>
+      </nav>
+    </div>
+  );
+}
