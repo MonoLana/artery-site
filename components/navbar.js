4ab164deffcad72245fb59a4a37848a3f4b6ab8c
Monolana <naufalx26@gmail.com>
2024-10-15 21:02:51 +0700

    adding early navbar UI

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,25 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <div>
-      <nav className={"bg-primary fixed w-full top-0 py-2 px-8"}>
-        <p>this is navbar</p>
-      </nav>
+    <div className={"flex flex-row bg-primary py-4 px-8 justify-between"}>
+      <div>
+        <a className="font-bold text-2xl text-white">Artery</a>
+      </div>
+      <div className={"text-white px-4 justify-between"}>
+        <Link href="#" className="px-4">
+          Cart
+        </Link>
+        <Link href="#" className="px-4">
+          Account
+        </Link>
+        <Link href="#" className="px-4">
+          Merchant
+        </Link>{" "}
+        <button className="rounded-lg bg-primaryButton px-6 py-2 font-bold">
+          Shop now!
+        </button>
+      </div>
     </div>
   );
 }
