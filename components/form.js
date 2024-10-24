6bef322942a53edeb2c4de51dc48fd7e6aae783f
Monolana <naufalx26@gmail.com>
2024-10-24 20:00:12 +0700

    custom some color on the form component

--- components/form.js
+++ components/form.js
@@ -1,13 +1,13 @@
 export default function Form() {
   return (
     <div className="min-h-screen flex items-center justify-center w-full">
-      <div className="shadow-md rounded-lg px-8 py-6 max-w-md">
+      <div className="shadow-md rounded-lg px-8 py-4 max-w-md">
         <h1 className="text-2xl font-bold text-center mb-4">Welcome!</h1>
         <form action="#">
           <div className="mb-4">
             <input
               type="email"
-              className="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500"
+              className="shadow-sm rounded-md w-full px-3 py-2 border border-primary focus:border-primary"
               placeholder="username"
               required
             />
@@ -15,17 +15,28 @@ export default function Form() {
           <div className="mb-4">
             <input
               type="password"
-              className="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500"
+              className="shadow-sm rounded-md w-full px-3 py-2 border border-primary focus:border-primary"
               placeholder="password"
               required
             />{" "}
           </div>
           <button
             type="submit"
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primaryButton hover:bg-secondaryButton focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Login
           </button>
+          <p className="flex text-black font-bold p-4">
+            <span>Don&apos;t have account yet? </span>
+            <span className="ml-2">
+              <a
+                href="#"
+                className="text-primaryText hover:text-secondaryText font-bold"
+              >
+                Register here
+              </a>
+            </span>
+          </p>
         </form>
       </div>
     </div>
